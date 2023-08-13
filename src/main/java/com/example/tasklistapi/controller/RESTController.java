343f07803d045c122d9dbd762df48c55f6072959
Tyan v sharfe <thisonlystudy@gmail.com>
2023-08-13 13:25:36 +0500

    Added REST methods

--- src/main/java/com/example/tasklistapi/controller/RESTController.java
+++ src/main/java/com/example/tasklistapi/controller/RESTController.java
@@ -1,7 +1,27 @@
 package com.example.tasklistapi.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class RESTController {
+    @GetMapping("/tasks")
+    public void getTask() {
+
+    }
+
+    @DeleteMapping("/tasks")
+    public void deleteTask() {
+
+    }
+
+    @PostMapping("/tasks")
+    public void addTask() {
+
+    }
+
+    @PutMapping("/tasks")
+    public void updateTask() {
+
+    }
 }
--- src/main/java/com/example/tasklistapi/model/Task.java
+++ src/main/java/com/example/tasklistapi/model/Task.java
@@ -1,18 +1,36 @@
 package com.example.tasklistapi.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Task {
     @Id
+    @GeneratedValue
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String description;
+
+    private boolean hasDone;
 
     public Long getId() {
         return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isHasDone() {
+        return hasDone;
+    }
+
+    public void setHasDone(boolean hasDone) {
+        this.hasDone = hasDone;
     }
 }
